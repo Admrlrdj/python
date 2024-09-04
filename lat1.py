700840a54f10d5c5c2f3e0b8ff178d43f93b7477
Rdjadmrl <radjaadmiral00@gmail.com>
2024-09-04 17:52:35 +0700

    update 4-9-2024

--- lat1.py
+++ lat1.py
@@ -1,35 +1,58 @@
-uts = float(input("Masukkan Nilai UTS: "))
-utsp = float(input("Masukkan Nilai UTSP: "))
-uas = float(input("Masukkan Nilai UAS: "))
-uasp = float(input("Masukkan Nilai UASP: "))
+##################################################################################
 
-hasil_uts = uts * 0.3
-hasil_utsp = utsp * 0.15
-hasil_uas = uas * 0.4
-hasil_uasp = uasp * 0.15
-hasil_akhir = hasil_uas + hasil_uasp + hasil_uts + hasil_utsp
+# bil1 = int(input("Masukkan integer pertama: "))
+# bil2 = int(input("Masukkan integer kedua: "))
 
-if hasil_akhir >= 80:
-    print("Nilai Akhir: ", hasil_akhir)
-    print("Nilai Mutu: A")
-elif hasil_akhir >= 70:
-    print("Nilai Akhir: ", hasil_akhir)
-    print("Nilai Mutu: B")
-elif hasil_akhir >= 60:
-    print("Nilai Akhir: ", hasil_akhir)
-    print("Nilai Mutu: C")
-elif hasil_akhir >= 40:
-    print("Nilai Akhir: ", hasil_akhir)
-    print("Nilai Mutu: D")
-else:
-    print("Nilai Akhir: ", hasil_akhir)
-    print("Nilai Mutu: E")
+# if bil1 == 7 or bil2 == 7:
+#     print("Hasil: Benar")
+# elif bil1 + bil2 == 7:
+#     print("Hasil: Benar")
+# elif bil1 - bil2 == 7:
+#     print("Hasil: Benar")
+# else:
+#     print("Hasil: Salah")
 
-# print(hasil_uts)
-# print(hasil_utsp)
-# print(hasil_uas)
-# print(hasil_uasp)
-# print(hasil_akhir)
+##################################################################################
+
+# huruf = input("Masukkan sebuah huruf: ")
+
+# vokal = ["a", "i", "u", "e", "o", "A", "I", "U", "E", "O"]
+
+# if huruf in vokal:
+#     print("Benar,", huruf, "adalah huruf vokal")
+# else:
+#     print("Salah,", huruf, "bukan huruf vokal")
+
+##################################################################################
+
+# uts = float(input("Masukkan Nilai UTS: "))
+# utsp = float(input("Masukkan Nilai UTSP: "))
+# uas = float(input("Masukkan Nilai UAS: "))
+# uasp = float(input("Masukkan Nilai UASP: "))
+
+# hasil_uts = uts * 0.3
+# hasil_utsp = utsp * 0.15
+# hasil_uas = uas * 0.4
+# hasil_uasp = uasp * 0.15
+# hasil_akhir = hasil_uas + hasil_uasp + hasil_uts + hasil_utsp
+
+# if hasil_akhir >= 80:
+#     print("Nilai Akhir: ", hasil_akhir)
+#     print("Nilai Mutu: A")
+# elif hasil_akhir >= 70:
+#     print("Nilai Akhir: ", hasil_akhir)
+#     print("Nilai Mutu: B")
+# elif hasil_akhir >= 60:
+#     print("Nilai Akhir: ", hasil_akhir)
+#     print("Nilai Mutu: C")
+# elif hasil_akhir >= 40:
+#     print("Nilai Akhir: ", hasil_akhir)
+#     print("Nilai Mutu: D")
+# else:
+#     print("Nilai Akhir: ", hasil_akhir)
+#     print("Nilai Mutu: E")
+
+##################################################################################
 
 # bil = int(input("Masukkan bilangan: "))
 
@@ -42,6 +65,8 @@ else:
 # elif bil % 2 == 1 and bil <= 0:
 #     print(bil, "Bilangan Ganjil Negatif")
 
+###################################################################################
+
 # if bil % 2 == 0:
 #     if bil >= 0:
 #         print(bil, "Bilangan Genap Positif")
@@ -53,6 +78,7 @@ else:
 #     else:
 #         print(bil, "Bilangan Ganjil Negatif")
 
+##################################################################################
 
 # if bil>10:
 #     print(bil, "lebih besar dari 10")
