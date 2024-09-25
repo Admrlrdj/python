dc1e764aa4043b041a3d8f11eff1a67c231fdca0
Rdjadmrl <radjaadmiral00@gmail.com>
2024-09-25 21:57:39 +0700

    QUIZ TAIIIIIIIIIIIIIIIIIIIIIIIIII

---
+++ Quiz tadi kek anjg/duabilanganprima.py
@@ -0,0 +1,24 @@
+angka = int(input("Masukkan angka: "))
+
+ketemu = False
+
+for i in range(2, angka):
+    prima1 = True
+    for j in range(2, int(i**0.5) + 1):
+        if i % j == 0:  
+            prima1 = False
+            break
+    
+    prima2 = True
+    for j in range(2, int((angka - i)**0.5) + 1):
+        if (angka - i) % j == 0:
+            prima2 = False
+            break
+    
+    if prima1 and prima2:
+        print("Iya,", i, "dan", angka - i, "adalah Prima")
+        ketemu = True
+        break
+    
+if not ketemu:
+    print("Tidak,", i, "dan", angka - 1, "bukan prima")
